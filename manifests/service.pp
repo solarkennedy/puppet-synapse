2ed12b582d704d1c229ba54090b0ffbcd04fe100
Kyle Anderson <kyle@xkyle.com>
2013-11-30 17:03:08 -0800

    Fix service registry check

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,14 +4,62 @@
 #
 # == Parameters
 #
-# [*port*]
-#   Port on the local side that the service is exposing. No default.
+# [*default_servers*]
+#  Fallback servers to use if synapse cannot discovery anything
+#  Must be an array of hashes:
+#
+#   default_servers => [
+#       {
+#         "name" => "default-db",
+#         "host" => "mydb.example.com",
+#         "port" => 5432
+#       }
+#     ]
+# 
+# [*discovery*]
+#  How synapse discovers hosts for the service.
+#  Must be a hash.
+#
+#    discovery => {
+#      "method" => "zookeeper",
+#      "path"   => "/airbnb/service/service2",
+#      "hosts"  => [
+#        "zk0.airbnb.com:2181",
+#        "zk1.airbnb.com:2181"
+#      ]
+#    },
+#
+# [*haproxy*]
+#  How synapse will configure haproxy's backend
+#  Must be a hash.
+#
+#    haproxy => {
+#      "port"           => '3214',
+#      "server_options" => "check inter 2s rise 3 fall 2",
+#      "listen"         => [
+#        "mode http",
+#        "option httpchk /health"
+#      ]
+#    }
+#
+# [*ensure*]
+#  Controls if the file is present or absent.
+#
+# [*target*]
+#  Defaults to /etc/synapse/conf.d/${name}.json
 #
 define synapse::service (
-  $target        = "/etc/synapse/conf.d/${name}.json"
+  $default_servers,
+  $discovery,
+  $haproxy,
+  $ensure = 'present',
+  $target = "/etc/synapse/conf.d/${name}.json",
 ) {
 
   include stdlib
+  validate_array($default_servers)
+  validate_hash($discovery)
+  validate_hash($haproxy)
   validate_absolute_path($target)
 
   file { $target:
@@ -19,6 +67,7 @@ define synapse::service (
     owner   => 'root',
     mode    => '0444',
     content => template('synapse/service.json.erb'),
+    notify  => Service['synapse'],
   }
 
 }
