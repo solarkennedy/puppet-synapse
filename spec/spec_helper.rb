f5ccff05063fc6d5fd866a8de65a3653152403bb
Josh Snyder <josnyder@yelp.com>
2016-05-25 09:54:37 -0700

    Make site.pp work with the future parser

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,19 @@
+require 'rspec/core/shared_context'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+module GlobalHelper
+  extend RSpec::Core::SharedContext
+
+  def global_pre_condition
+    [(File.read('spec/fixtures/manifests/site.pp') rescue ''),
+     'Package { provider => "apt" }']
+  end
+
+  if ENV['FUTURE_PARSER'] == 'yes' or not Puppet.version =~ /^3/
+    let(:pre_condition) { global_pre_condition }
+  end
+end
+
+RSpec.configure do |c|
+  c.include GlobalHelper
+end
